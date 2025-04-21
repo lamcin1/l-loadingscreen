fba8ee9d5399c321628193b144055f8ecd51e5fe
LaMcIn / Nico <43954221+lamcin1@users.noreply.github.com>
2025-04-21 09:17:07 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+
+files {
+    'html/index.html',   
+    'html/*.json',    
+    'html/img/*',
+    'html/video/*',
+
+}
+
+loadscreen {
+  'html/index.html'
+}
+
+loadscreen_cursor 'yes'
